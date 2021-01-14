8ddb116f4a6e902106a4bd205d54fdbca99a182f
Eamon Boyle <blaowskate@hotmail.com>
2021-01-14 20:16:15 +0000

    Initial setup, with template.

---
+++ Roguelike_Template/PCH.h
@@ -0,0 +1,9 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <sstream>
+#include <fstream>
+#include <memory>
+
+#include "Util.h"
+#include "TextureManager.h"
+#include "SoundBufferManager.h"
